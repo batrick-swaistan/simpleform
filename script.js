16855e31b3128e922f1cbc91db907a79385a634e
batrick-swaistan <batrickswaistan@gmail.com>
2024-02-02 14:48:00 +0530

    Naming Conflicit Cleared

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
 
+    var x = document.getElementById('login');
+    var y = document.getElementById('register');
+    var z = document.getElementById('btn');
+
+    function switchToLogin() {
+        x.style.left = "27px";
+        y.style.right = "-350px";
+        z.style.left = "0px";
+    }
+
+    function switchToRegister() {
+        x.style.left = "-350px";
+        y.style.right = "25px";
+        z.style.left = "150px";
+    }
+
+    function myLogPassword() {
+        var a = document.getElementById("loginPassword");
+        var b = document.getElementById("eye");
+        var c = document.getElementById("eye-slash");
+
+        if (a.type === "password") {
+            a.type = "text";
+            b.style.opacity = "0";
+            c.style.opacity = "1";
+        } else {
+            a.type = "password";
+            b.style.opacity = "1";
+            c.style.opacity = "0";
+        }
+    }
+
+    function myRegPassword() {
+        var d = document.getElementById("signupPassword");
+        var b = document.getElementById("eye-2");
+        var c = document.getElementById("eye-slash-2");
+
+        if (d.type === "password") {
+            d.type = "text";
+            b.style.opacity = "0";
+            c.style.opacity = "1";
+        } else {
+            d.type = "password";
+            b.style.opacity = "1";
+            c.style.opacity = "0";
+        }
+    }
+
+
 var users = JSON.parse(localStorage.getItem('users')) || [];
 
 
@@ -50,53 +99,6 @@ function login(event) {
 }
 
 
-var x = document.getElementById('login');
-var y = document.getElementById('register');
-var z = document.getElementById('btn');
-
-function login() {
-    x.style.left = "27px";
-    y.style.right = "-350px";
-    z.style.left = "0px";
-}
-
-function register() {
-    x.style.left = "-350px";
-    y.style.right = "25px";
-    z.style.left = "150px";
-}
-
-function myLogPassword() {
-    var a = document.getElementById("logPassword");
-    var b = document.getElementById("eye");
-    var c = document.getElementById("eye-slash");
-
-    if (a.type === "password") {
-        a.type = "text";
-        b.style.opacity = "0";
-        c.style.opacity = "1";
-    } else {
-        a.type = "password";
-        b.style.opacity = "1";
-        c.style.opacity = "0";
-    }
-}
-
-function myRegPassword() {
-    var d = document.getElementById("regPassword");
-    var b = document.getElementById("eye-2");
-    var c = document.getElementById("eye-slash-2");
-
-    if (d.type === "password") {
-        d.type = "text";
-        b.style.opacity = "0";
-        c.style.opacity = "1";
-    } else {
-        d.type = "password";
-        b.style.opacity = "1";
-        c.style.opacity = "0";
-    }
-}
 
 
 function logout() {
